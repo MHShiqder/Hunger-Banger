4afde2550071f5e8e858e9735236d02fe530db1d
Hasibul Hasan <hasibul277127@gmail.com>
2025-01-09 22:48:13 +0600

    added dynaming title

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,14 @@ import Category from './Category/Category';
 import PopularMenu from './PopularMenu/PopularMenu';
 import FeaturedItem from './FeaturedItem/FeaturedItem';
 import Rivews from './Reviews/Rivews';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home | Hunger Banger</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
